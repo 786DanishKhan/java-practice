484ab28ed59ecf1a8254e4ec5348a83c77de75eb
786DanishKhan <danish091098@gmail.com>
2024-12-02 15:07:38 +0530

    Add new program

---
+++ src/EvenNumber.java
@@ -0,0 +1,10 @@
+public class EvenNumber {
+    public static void main(String[] args) {
+        int numbers = 78;
+if (numbers%2==0){
+    System.out.println("Print number is even:" +numbers);
+}else{
+    System.out.println("Print number is not even:" +numbers);
+}
+    }
+}
---
+++ src/OddNumber.java
@@ -0,0 +1,12 @@
+public class OddNumber {
+    public static void main(String[] args) {
+        int numbers = 38;
+
+        if(numbers%2!=0){
+            System.out.println("Print number is Odd:" +numbers);
+        } else{
+            System.out.println("Print number is not Odd:" +numbers);
+        }
+
+    }
+}
