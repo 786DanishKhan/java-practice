23b6371ef82970d6d5058a2a0ccb1aa3c2edf62c
786DanishKhan <danish091098@gmail.com>
2024-12-21 12:56:30 +0530

    added practice package

---
+++ src/practice/CheckEqualArray.java
@@ -0,0 +1,21 @@
+package practice;
+
+import java.util.Arrays;
+
+public class CheckEqualArray {
+    public static void main(String[] args) {
+        int  [] arr1 = {1, 2, 5, 4, 0};
+        int [] arr2 = {2, 4, 5, 0, 1};
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                System.out.println("false");
+            }else{
+                System.out.println("true");
+            }
+        }
+    }
+}
